93bbfbb2477042c811058757a7bd264ff9750c9a
apple <2855289441@qq.com>
2021-04-08 09:37:35 +0800

    工厂方法模式

--- src/factoryMethod2/Client.java
+++ src/factoryMethod2/Client.java
@@ -1,19 +1,20 @@
 package factoryMethod2;
 
+import factoryMethod2.utils.XMLUtil;
+
 import java.io.IOException;
 
 //客户
 public class Client {
     public static void main(String[] args) throws IOException {
         //1、文件日志
-        LoggerFactory fileLoggerFactory = new FileLoggerFactory();
+        //LoggerFactory fileLoggerFactory = new FileLoggerFactory();
+        FileLoggerFactory fileLoggerFactory = (FileLoggerFactory)XMLUtil.getFactory();
         FileLogger fileLogger = (FileLogger) fileLoggerFactory.createLogger();
         fileLogger.writeLog();
         //2、数据库日志
         LoggerFactory DBloggerFactory = new DBLoggerFactory();
         DBLogger dbLogger = (DBLogger) DBloggerFactory.createLogger();
         dbLogger.writeLog();
-
-        //
     }
 }
--- src/factoryMethod2/utils/XMLUtil.java
+++ src/factoryMethod2/utils/XMLUtil.java
@@ -1,6 +1,8 @@
 package factoryMethod2.utils;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -8,15 +10,28 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 public class XMLUtil {
     //从XML配置文件中提取具体的类名，并返回一个实例
-    public static Object getFactory() throws ParserConfigurationException, IOException, SAXException {
+    public static Object getFactory(){
+        Object object = null;
+        try{
+        //创建DOM文档对象
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse(new File("./config/config.xml"));
+        Document document = documentBuilder.parse(new File("/Users/apple/Documents/java设计模式/模式代码/src/factoryMethod2/config/config.xml"));
+        //获取相应的文本节点（类名）
+        NodeList nodeList = document.getElementsByTagName("className");
+        Node classNode = nodeList.item(0).getFirstChild();
+        String className = classNode.getNodeValue();
+        //反射对象
+        Class c = Class.forName(className);
+        object = c.getDeclaredConstructor().newInstance();
 
-
-        return new Object();
+        }catch (Exception e) {
+            System.out.println("工厂创建异常");
+        }
+        return object;
     }
 }
